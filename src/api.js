2c0f52151274fbfc2a3e39ca0f546195afc1e377
Jap <jap@wsl>
2022-11-03 22:59:38 +1300

    deploy in netlify

--- src/api.js
+++ src/api.js
@@ -1,28 +1,40 @@
-import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import * as dotenv from 'dotenv'
 import express from 'express';
 import { createClient } from '@supabase/supabase-js';
+import serverless from 'serverless-http';
 
 dotenv.config();
 
 const app = express();
+const router = express.Router();
 const supabaseUrl = 'https://bdivylzuoyjsbxdaclam.supabase.co';
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-app.get("/", (req, res) => {
+// GET api/
+router.get("/", (req, res) => {
   const fetchSales = async () => {
     const { data, error } = await supabase
       .from('sales_history')
       .select()
       .limit(10);
     if (error) {
-      res.send(error);
+      res.json(error);
       return false
     }
-    res.send(data);
+    res.json(data);
   }
 
   fetchSales();
 });
 
-app.listen(9999, () => { console.log("API is running on 9999") });
+// GET api/monthly
+router.get("/monthly", (req, res) => {
+  res.json({
+    "success": "This route works"
+  });
+});
+
+app.use('/.netlify/functions/api', router);
+
+exports.handler = serverless(app);
